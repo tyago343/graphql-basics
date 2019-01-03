50680166f547fba6ddd8e7dbe448f06f52acb139
tyago343 <tyagocasanova@hotmail.com>
2019-01-02 23:13:18 -0300

    class init

---
+++ src/index.js
@@ -0,0 +1,102 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+//scalar types - String, integer, float, boolean, ID
+//Demo user data
+const users = [{
+    id : '1',
+    name : 'Santiago',
+    email : 'santiago@mail.com',
+    age : 26
+},{
+    id : '2',
+    name : 'Sarah',
+    email : 'sarah@mail.com',
+    age : 22
+},{
+    id : '3',
+    name : 'Pat',
+    email : 'pat@mail.com'
+}]
+const posts = [{
+    id : '1',
+    title : 'título 1',
+    body : 'Body 1',
+    published : false
+},{
+    id : '2',
+    title : 'título 2',
+    body : 'Body 2',
+    published : false
+},{
+    id : '3',
+    title : 'título 3',
+    body : 'Body 3',
+    published : true
+}]
+//type definitions (schema)
+const typeDefs = `
+    type Query {
+        users(query: String) : [User!]!
+        posts(query: String) : [Post!]!
+        post : Post!
+        me : User!
+    }
+    type User {
+        id : ID!
+        name : String!
+        email : String!
+        age : Int
+    }
+    type Post {
+        id : ID!
+        title : String!
+        body : String!
+        published : Boolean!
+    }
+` 
+//Resolvers
+const resolvers = {
+    Query: {
+        posts(parent, args, ctx, info){
+            if(!args.query){
+                return posts
+            }
+            return posts.filter(post=>{
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
+        },
+        users(parent, args, ctx, info){
+            if(!args.query){
+                return users
+            }
+            return users.filter(user=>{
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        post() {
+           return {
+               id : '123098',
+               title : 'A post title',
+               body : 'This is a post body, here we need to put some content',
+               published : false
+           }
+        },
+        me() {
+            return {
+               id : '123098',
+               name : 'Santiago',
+               email : 'santiago@htoma.com'
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+server.start(()=>{
+    console.log('The server is up!')
+})
